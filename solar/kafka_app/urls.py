71136b0f8bfafe6f8e63abae469b14df16bf5213
Sparsh Bohra <abc@example.com>
2024-09-11 17:06:44 +0530

    .

--- solar/kafka_app/urls.py
+++ solar/kafka_app/urls.py
@@ -1,7 +1,8 @@
 # kafka_app/urls.py
 from django.urls import path
-from .views import start_kafka_consumer
+from .views import *
 
 urlpatterns = [
     path('start-consumer/', start_kafka_consumer, name='start_kafka_consumer'),
+    path('start-consumer-2/', start_weather_consumer, name='start_weather_consumer'),
 ]
