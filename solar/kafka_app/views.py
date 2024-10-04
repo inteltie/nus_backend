2de58c7d9bf043bb7911e17ef5f6375b193f7432
Sparsh Bohra <abc@example.com>
2024-10-04 12:52:10 +0530

    .

--- solar/kafka_app/views.py
+++ solar/kafka_app/views.py
@@ -3,6 +3,7 @@ from .tasks import run_kafka_consumer, run_weather_consumer
 import csv
 import os
 from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 CSV_LOG_FILE = os.path.join(BASE_DIR, 'kafka_app', 'alert_logs.csv')
@@ -15,19 +16,25 @@ def start_weather_consumer(request):
     run_weather_consumer()
     return JsonResponse({"status": "Weather consumer started"})
 
+@csrf_exempt
 def get_alert_logs(request):
     data = []
     with open(CSV_LOG_FILE, 'r') as file:
         reader = csv.reader(file)
         for row in reader:
-            data.append({
+            log_entry = {
                 "log_timestamp": row[0],
                 "data_timestamp": row[1],
-                "out_of_range_values": row[2:]
-            })
+                "title": row[2],
+                "description": row[3],
+                "out_of_range_variables": [
+                    var for var in row[4:]  # This assumes remaining columns are out-of-range variables
+                ]
+            }
+            data.append(log_entry)
     return JsonResponse(data, safe=False)
 
-
+@csrf_exempt
 def delete_alert(request, alert_id):
     # Read current logs
     data = []
@@ -45,7 +52,7 @@ def delete_alert(request, alert_id):
     else:
         return JsonResponse({"status": "Alert ID not found"}, status=404)
 
-
+@csrf_exempt
 def delete_all_alerts(request):
     # Clear the CSV file
     open(CSV_LOG_FILE, 'w').close()  # This will truncate the file
