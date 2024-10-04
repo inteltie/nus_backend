b0c935b5fc3211d0488b8904dc3a16ffe71ea11f
Sparsh Bohra <abc@example.com>
2024-10-04 13:06:07 +0530

    .

--- solar/kafka_app/urls.py
+++ solar/kafka_app/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('start-consumer/', start_kafka_consumer, name='start_kafka_consumer'),
     path('start-weather-consumer/', start_weather_consumer, name='start_weather_consumer'),
     path('logs/', get_alert_logs, name='get_alert_logs'),
-    path('delete_alert/<int:alert_id>/', delete_alert, name='delete_alert'),
+    path('delete_alert/<uuid:alert_id>/', delete_alert, name='delete_alert'),
     path('delete_all_alerts/', delete_all_alerts, name='delete_all_alerts'),
 ]
